b27269c00c155f1d8decc963ad9715a56bd4573d
Dylan Liesenfelt <djliesenfelt@gmail.com>
2025-02-27 00:28:33 -0500

    build: init

---
+++ src/store.js
@@ -0,0 +1,6 @@
+const store_button = document.getElementById('store-btn')
+const store_display = document.getElementById('store')
+
+store_button.addEventListener('click', () => {
+    store_display.style.visibility = 'visible'
+})
