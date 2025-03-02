aec3293b1bc255786b27e4c9553fc461aa072fca
Dylan Liesenfelt <djliesenfelt@gmail.com>
2025-03-01 19:51:30 -0500

    build: combat()

--- src/combat.js
+++ src/combat.js
@@ -1,20 +1,25 @@
-function updateInfo() {
-    
+function checkHealth() {
+    if (health === 0) {
+        reward()
+    } else {
+        requestAnimationFrame(checkHealth)
+    }
 }
+
 
 function combat(health, clickMod) {
     
     const startHealth = health
-
-    if (health > 0) {
-        enemy_card.addEventListener('click', () => { // Click attack
+    
+    enemy_card.addEventListener('click', () => { // Click attack
+        if (health > 0) {
             health = health - 1*clickMod
             enemy_hp.innerText = health
             let barWidth = ((health/startHealth) * 100) + '%' 
-            hp_bar.style.width = barWidth    
-        })
-    } else {
-        console.log('dead')
-        return
-    }   
+            hp_bar.style.width = barWidth
+              
+        }
+    })
+    
+    checkHealth()
 }
