0a28ce68cafac5343b5510dfe9b679af8834aec0
Dylan Liesenfelt <djliesenfelt@gmail.com>
2025-03-03 15:02:35 -0500

    Add: Card Images

--- src/game.js
+++ src/game.js
@@ -11,45 +11,35 @@ const coin_display = document.getElementById('coin')
 // ===== Lists =====
 //Adjectives
 const adj = [
-    'Colossal', 'Frozen', 'Chaos', 'Skeletal', 'Twilight',
-    'Molten', 'Slimy', 'Hairy', 'Unholy', 'Elder',
-    'Electric', 'Drunk', 'Juvenile', 'Paper', 'Holy', 
-    'Earthen', 'Arcane','Deadly', 'Feral', 'Metal',
-    'Aquatic', 'Primal', 'Celestial', 'Evolved',  
-    'Storm', 'Crystal', 'Wooden', 'Hallucinogenic',
-    'Glitched', 'Shadow', 'Pale', 'Quantum',
-    'Radioactive', 'Battle', 'Void', 'Parasitic',
-    'Elemental', 'Undead', 'Psychic','Exotic', 'Doctor', 
-    'Cyborg', 'Ancestral', 'Sand', 'Buff','Nature', 
-    'Death', 'Soul', 'Chromatic', 'Stone', 'Flame', 
-    'Eldritch', 'Runic', 'Cartoon', 'Plasma', 'Nightmare',
-    'Obsidian', 'Plastic'
+'Arcane','Battle','Buff','Crystal','Cyborg',
+'Drunk','Eldritch','Feral','Fossil','Frozen',
+'Glitched','Holy','Inferno','Liquid','Golden',
+'Nightmare','Paper','Plastic','Plasma','Primal',
+'Psychic','Radioactive','Sand','Steampunk','Stone',
+'Stone','Storm','Twilight','Undead','Void'
 ]
 
 //Nouns
 const names = [
-    'Rodent','Ox','Tiger','Rabbit','Dragon','Sepent','Stallion',
-    'Ram','Ape','Wolf','Boar','Toad','Robot',
-    'Dinosaur','Penguin','Owl','Demon','Angel',
-    'Goblin','Bug','Bat','Slug','Slime','Bear','Racoon',
-    'Ninja','Tank','Hydra','Army','Stag','Shark','Whale',
-    'Troll','Dwarf','Elf','Ghost','Ghoul','Hawk',
-    'Guardian','Bigfoot', 'God','Turtle','Golem','Walker','Monk', 
-    'Mage','Spider','Destroyer','Vampire','Kraken','Beast', 
-    'Queen', 'King','Paladin','Gladiator','Puppet','Lich','Clown'
+'Angel','Bear','Bigfoot','Demon','Dinosaur',
+'Dragon','Ghoul','Goblin','God','Golem',
+'Hawk','Hydra','Kraken','Lich','Mage',
+'Monk','Paladin','Penguin','Puppet','Racoon',
+'Ram','Rodent','Shark','Slug','Spider',
+'Tank','Tiger','Toad','Turtle','Wolf'
 ]
 
 // ===== Player Vars =====
 let level = 0
 let clickMod = 1
+let goldMod = 1
+let autoMod = 1
 let coin = 0
 let playerHand = []
 
 // ===== Enemy Vars =====
 let enemy
 let health = 0
-
-
 
 function game() {
     if (level === 58) {
